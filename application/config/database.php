9ad2440ce0e3532a60a5002acf3101dc007be666
Bergerivan <bergerivan@mail.ru>
2012-08-20 12:02:52 +0300

    sdf

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'admin';
-$db['default']['password'] = 'admin';
-$db['default']['database'] = 'fundraiser';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'local_fundraiser';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
